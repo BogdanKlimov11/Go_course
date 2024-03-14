94fe715f8436557dd56471f929ad69ace3c3bc49
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-03-14 08:27:08 +0300

    Create Selection_sort.go

---
+++ Sorting_algorithms/Selection_sort.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func selectionSort(arr []int) {
+    n := len(arr)
+    for i := 0; i < n-1; i++ {
+        minIndex := i
+        for j := i + 1; j < n; j++ {
+            if arr[j] < arr[minIndex] {
+                minIndex = j
+            }
+        }
+        if minIndex != i {
+            arr[i], arr[minIndex] = arr[minIndex], arr[i]
+        }
+    }
+}
